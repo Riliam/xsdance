97ef89a176a808333130b0d65a3f22469c84a989
riliam <alexey.milograodv@gmail.com>
2015-11-20 23:59:45 +0200

    Add validation

---
+++ utils.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+import re
+import json
+
+from lxml import etree
+
+
+tree = lambda: defaultdict(tree)
+
+re_inline_parent_name = re.compile(r'([a-zA-Z0-9]+)#\{\1:([0-9]+)\}')
+
+
+def make_exception(name):
+    return type(name, (BaseException,), {})
+
+
+def add(d, keys, value, process_input=lambda value, _: value):
+    for i, key in enumerate(keys):
+        # check if current key looks like inline parent
+        # if true, switch `process_input` function to support lists
+        m = re_inline_parent_name.match(key)
+        if m:
+            key = m.group(1)
+            index = m.group(2)
+            process_input =\
+                lambda value, content: dict({index: value}, **content)
+        # create empty dict for given keyif not last element
+        # or add value, if element is last
+        last_element = (i == len(keys) - 1)
+        if last_element:
+            d[key] = process_input(value, d[key])
+        else:
+            d = d[key]
+
+
+def parse_inputs(inputs, divider='__'):
+    d = tree()
+    for name, value in inputs.items():
+        add(d, name.split(divider), value)
+    return json.loads(json.dumps(d))
+
+
+def _serialize_xml(d, root=None):
+    root = root if isinstance(root, etree._Element) else etree.Element(root)
+    for name, value in d.items():
+        elem = etree.Element(name)
+        if isinstance(value, dict):
+            serialize_xml(value, elem)
+        else:
+            elem.text = value
+        root.append(elem)
+    return root
+
+
+def serialize_xml(d, root='xml'):
+    return etree.tostring(_serialize_xml(d, root))
--- xsdance.py
+++ xsdance.py
@@ -1,9 +1,21 @@
+import json
+
+from utils import serialize_xml, make_exception
+
+
 class Element(object):
+
+    ValueRequiredError = make_exception('ValueRequiredError')
+    ValidationError = make_exception('ValidationError')
 
     nesting_connector = '__'
     default_input = '<input name="{name}" value="{value}"/>'
     default_wrapper = '<div data-element={name}>{content}<span class="error"></span></div>'  # NOQA
-    parent_element_wrapper = '<div style="border: 1px solid black;"><h5>{parent_name}</h5>{content}</div>'
+    parent_element_wrapper = '<div style="border: 1px solid black;"><h5>{parent_name}</h5>{content}</div>'  # NOQA
+
+    error_messages = {
+        'required': 'This field is required',
+    }
 
     def __init__(self, name, value=None, label_text='',
                  html_input=default_input,
@@ -28,6 +40,16 @@ class Element(object):
     def __repr__(self):
         return self.name
 
+    @property
+    def prefixed_name(self):
+        name = self.name
+        if self.parent:
+            name = '{prefix}{connector}{name}'.format(
+                prefix=self._get_full_prefix(),
+                connector=self.nesting_connector,
+                name=name)
+        return name
+
     def render_html(self):
         content = self._render_subelements_html()\
             or self._html_input_with_value()
@@ -36,10 +58,34 @@ class Element(object):
             content=content)
 
     def render_xml(self):
-        pass
+        if not self.value:
+            raise Element.ValueRequiredError
+        return serialize_xml(self.value)
 
     def render_json(self):
-        pass
+        if not self.value:
+            raise Element.ValueRequiredError
+        return json.dumps(self.value)
+
+    def validate(self, d):
+        errors = {}
+        value = d[self.name]
+        for sub in self.subelements:
+            v = value.get(sub.name, None)
+            if v is None and sub.min_occurs > 0:
+                errors[sub.prefixed_name] = [self.error_messages['required']]
+            else:
+                suberrors = sub.validate(value)
+                if suberrors:
+                    errors.update(suberrors)
+
+        for validator in self.validators:
+            result = validator(value)
+            if result:
+                errors[self.prefixed_name] =\
+                    errors.get(self.prefixed_name, []) + [result]
+
+        return errors
 
     def _html_input_with_value(self):
         name = self.prefixed_name
@@ -49,7 +95,6 @@ class Element(object):
 
     def _render_subelements_html(self):
         content = ''.join(el.render_html() for el in self.subelements)
-        print(self.parent, content)
         if content:
             content = self.parent_element_wrapper.format(
                 parent_name=self.name,
@@ -66,13 +111,3 @@ class Element(object):
             parents.append(el.parent.name)
             el = el.parent
         return self.nesting_connector.join(reversed(parents))
-
-    @property
-    def prefixed_name(self):
-        name = self.name
-        if self.parent:
-            name = '{prefix}{connector}{name}'.format(
-                prefix=self._get_full_prefix(),
-                connector=self.nesting_connector,
-                name=name)
-        return name
